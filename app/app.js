ec59b31fe60498ed9955eb02c0aba43647221f6d
SpanFish11 <spanfish93@gmail.com>
2024-10-10 11:38:43 +0200

    Added templated response

--- app/app.js
+++ app/app.js
@@ -2,9 +2,15 @@ import express from "express";
 import config from "./config/config.js";
 import meteorRouter from "./delivery/meteor_routes.js";
 import errorHandler from "./middleware/error_handler.js";
+import nunjucks from "nunjucks";
 
 const app = express();
 const port = config.port;
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+})
 
 app.use(express.json());
 
